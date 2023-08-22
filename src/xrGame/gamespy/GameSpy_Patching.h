5ba0a325fd3567529f90e89bd528e9ba4fc61529
OldSerpskiStalker <ictopiar@gmail.com>
2023-08-22 15:27:26 +0500

    Херня

--- src/xrGame/gamespy/GameSpy_Patching.h
+++ src/xrGame/gamespy/GameSpy_Patching.h
@@ -5,23 +5,22 @@
 class CGameSpy_Patching
 {
 private:
-    HMODULE m_hGameSpyDLL;
+	HMODULE	m_hGameSpyDLL;
 
-    void LoadGameSpy(HMODULE hGameSpyDLL);
-
+	void	LoadGameSpy(HMODULE hGameSpyDLL);
 public:
-    CGameSpy_Patching();
-    CGameSpy_Patching(HMODULE hGameSpyDLL);
-    ~CGameSpy_Patching();
+	CGameSpy_Patching();
+	CGameSpy_Patching(HMODULE hGameSpyDLL);
+	~CGameSpy_Patching();
 
-    void CheckForPatch(bool InformOfNoPatch);
-    void PtTrackUsage(int userID);
-
+	void CheckForPatch	(bool InformOfNoPatch);
+	void PtTrackUsage	(int userID);
 private:
-    //--------------------- GCD_Client -------------------------------------------
-    GAMESPY_FN_VAR_DECL(bool, ptCheckForPatchA,
-        (
-            //		int productID,  const char * versionUniqueID,  int distributionID,
-            ptPatchCallback callback, PTBool blocking, void* instance));
-    GAMESPY_FN_VAR_DECL(bool, ptTrackUsageA, (int userID));
+	//--------------------- GCD_Client -------------------------------------------	
+	GAMESPY_FN_VAR_DECL(bool, ptCheckForPatchA, (
+//		int productID,  const char * versionUniqueID,  int distributionID, 
+		ptPatchCallback callback, 
+		PTBool blocking, 
+		void * instance ));
+	GAMESPY_FN_VAR_DECL(bool, ptTrackUsageA,	(int userID));
 };
